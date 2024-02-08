4f785d33a11e72e1933eda456ba44d0067410964
Ose Ogbe <therealnylat@gmail.com>
2024-02-08 23:49:46 +0100

    nav menu

---
+++ components/navbar/Logo.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const Logo = () => {
+    const router = useRouter();
+
+    return (
+        // <Image
+        //     src="/images/logo.png"
+        //     alt="green flats logo"
+        //     height="100"
+        //     width="100"
+        //     className="hidden md:block cursor-pointer"
+        // />
+        <h1 className="hidden md:block text-3xl font-bold">
+            <span className="text-green-700">Green</span>flats<span className="text-green-700">.</span>
+        </h1>
+    )
+}
+
+export default Logo
