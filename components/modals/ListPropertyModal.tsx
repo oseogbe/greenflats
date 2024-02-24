944e3f7f9244830608094760cbb22672d60acdc3
Ose Ogbe <therealnylat@gmail.com>
2024-02-24 13:05:30 +0100

    list property: upload image with cloudinary

--- components/modals/ListPropertyModal.tsx
+++ components/modals/ListPropertyModal.tsx
@@ -11,6 +11,7 @@ import CountrySelect from "../inputs/CountrySelect";
 import useListPropertyModal from "@/hooks/useListPropertyModal";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS {
     CATEGORY = 0,
@@ -54,6 +55,7 @@ const ListPropertyModal = () => {
     const guestCount = watch('guestCount');
     const roomCount = watch('roomCount');
     const bathroomCount = watch('bathroomCount');
+    const imageSrc = watch('imageSrc');
 
     const Map = useMemo(() => dynamic(() => import('../Map'), {
         ssr: false
@@ -158,6 +160,21 @@ const ListPropertyModal = () => {
                     subtitle="How many bathrooms do you have?"
                     value={bathroomCount}
                     onChange={(value) => setCustomValue('bathroomCount', value)}
+                />
+            </div>
+        )
+    }
+
+    if (step === STEPS.IMAGES) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Add a photo of your place"
+                    subtitle="Show guests what your place looks like!"
+                />
+                <ImageUpload
+                    value={imageSrc}
+                    onChange={(value) => setCustomValue('imageSrc', value)}
                 />
             </div>
         )
